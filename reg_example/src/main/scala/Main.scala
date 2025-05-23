1d06f0d7e7dddbc3bd9d90c6f565915c54f87a7b
Maria_Isaeva_506 <maria.isaeva85@gmail.com>
2025-05-23 16:50:47 +0300

    Done the Res_test code. Was generated SV file

--- reg_example/src/main/scala/Main.scala
+++ reg_example/src/main/scala/Main.scala
@@ -1,15 +1,18 @@
 import chisel3._
-
+import _root_.circt.stage.ChiselStage
 
 
 class Reg_test extends Module {
+ private val aWidth = 4
+  private val bWidth = 4
+  private val sumWidth = aWidth + 1
+  private val resWidth = sumWidth + bWidth + 1  
+
   val io = IO(new Bundle {
-    val a = Input (UInt(4.W))
-    val b = Input (UInt(4.W))
-    val res = Output (UInt(resWidth.W))
+    val a = Input(UInt(aWidth.W))
+    val b = Input(UInt(bWidth.W))
+    val res = Output(UInt(resWidth.W))  
   })
-  private val sumWidth = io.a.getWidth + 1
-  private val resWidth = sumWidth + io.b.getWidth + 1
 
   val sum      = io.a +& io.b
   val regb     = RegInit(0.U(4.W))
@@ -19,9 +22,17 @@ class Reg_test extends Module {
   regb     := io.b
   io.res   := res_reg
   res_reg  := regsum +& regb
+  
+
 }
 
+
 object Reg_test_mod extends App {
-  emitVerilog(new Reg_test, 
-    Array("--target-dir", "../generated_SV_files/mux4_2/"))
+
+  ChiselStage.emitSystemVerilogFile(new Reg_test, 
+  args = Array("--target-dir", "../generated_SV_files/Reg_test/"),
+  firtoolOpts = Array("-disable-all-randomization",
+                      "-strip-debug-info",
+                      "--verify-each=false",
+                      "-default-layer-specialization=enable"))
 }
